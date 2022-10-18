cf7436a830e50f4ba08896b0774804b9c6b787b2
benjamin wiriyapong <bensw@ben-mc-2022.local>
2022-10-18 13:46:01 +0700

    first commit

---
+++ twitter_blocks/twitter_block_selenium.py
@@ -0,0 +1,4 @@
+from selenium.webdriver.support.relative_locator import locate_with
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
